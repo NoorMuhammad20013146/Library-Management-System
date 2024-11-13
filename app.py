0161d785fb489222f4b5f0948b5a2ba6df4311cc
Noor Muhammad <20013146@mydbs.ie>
2024-11-13 19:59:48 +0000

    MySQL Connected

--- app.py
+++ app.py
@@ -11,3 +11,6 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DB'] = 'bookstore'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
